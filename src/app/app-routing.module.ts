0b7d7ad4de57534b3f4f04820a9dc62685ee475d
Avellow <avellow@yandex.ru>
2023-06-25 21:54:04 +0300

    feat: added login and meetups routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
+import { MeetupsListComponent } from './modules/meetups-list/meetups-list.component';
+import { authGuard } from './shared/guards/auth.guard';
 
-const routes: Routes = [];
+export enum RoutePathsEnum {
+  LOGIN = 'login',
+  MEETUPS = 'meetups'
+}
+
+const routes: Routes = [
+  {path: RoutePathsEnum.LOGIN, component: LoginComponent},
+  {path: RoutePathsEnum.MEETUPS, component: MeetupsListComponent, canActivate: [authGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
