1a9b50eb575949cc081bcca0b96f0049d14e002c
Avellow <avellow@yandex.ru>
2023-06-27 16:17:28 +0300

    feat: added onclick emit reaction

--- src/app/modules/button-icon/button-icon.component.ts
+++ src/app/modules/button-icon/button-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IconLinksEnum, IconName } from './button-icon.types';
 
 @Component({
@@ -9,10 +9,15 @@ import { IconLinksEnum, IconName } from './button-icon.types';
 export class ButtonIconComponent {
   @Input() icon!: IconName;
   @Input() bordered = false;
-
+  @Output() btnClick = new EventEmitter();
+  
   get link(): string {
     const key = this.icon;
     return IconLinksEnum[key];
+  }
+
+  onClick() {
+    this.btnClick.emit();
   }
 
   get isAction(): boolean {
